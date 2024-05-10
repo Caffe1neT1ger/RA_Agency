8886be26e3f9745f5fa77676a6c50d4905100e21
Kalmykov Azamat <azamat.kalmykov07@gmail.com>
2024-05-10 20:45:26 +0300

    fix

--- middleware/ErrorHandlingMiddlware.js
+++ middleware/ErrorHandlingMiddlware.js
@@ -5,6 +5,6 @@ export const errorMiddlware = (error, req, res, next) => {
   if (error instanceof ApiError) {
     return res.status(error.status).json({ message: error.message, errors: error.errors });
   }
-  // return res.status(500).json({ message: 'Непредвиденная ошибка' });
-  return next(error);
+  return res.status(500).json({ message: 'Непредвиденная ошибка' });
+  // return next(error);
 };
